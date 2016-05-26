b975bb883f12ad4fbe4746191347a1355e84bf5b
Corey Yaspan <cspan00@gmail.com>
2016-05-26 14:19:46 -0600

    fix showing kids on profile

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -7,7 +7,7 @@ app.controller("profileController", function($scope, $http, posts, $location, $r
         $scope.posts = result;
         console.log($scope.posts);
     })
-    posts.getKids(result.facebook_id).then(function(result){
+    posts.getKidsbyFBId(result.facebook_id).then(function(result){
       console.log(result);
       $scope.kids = result
     })
