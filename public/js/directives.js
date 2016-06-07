15d0a0c4bd5f96e6ea3727e44e5bf8e0c9ed443e
Brandon Jewell <brandon.royce.jewell@gmail.com>
2016-06-06 20:37:40 -0600

    check it

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,33 +1,15 @@
-$(".dropdown dt a").on('click', function() {
-  $(".dropdown dd ul").slideToggle('fast');
-});
+angular
+  .module('myApp')
+  .directive('scrollToItem', function() {
+      return {
+          restrict: 'A',
+          scope: {
+              scrollTo: "@"
+          },
+          link: function(scope, $elm ,attr) {
 
-$(".dropdown dd ul li a").on('click', function() {
-  $(".dropdown dd ul").hide();
-});
-
-function getSelectedValue(id) {
-  return $("#" + id).find("dt a span.value").html();
-}
-
-$(document).bind('click', function(e) {
-  var $clicked = $(e.target);
-  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
-});
-
-$('.mutliSelect input[type="checkbox"]').on('click', function() {
-
-  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-    title = $(this).val() + ",";
-
-  if ($(this).is(':checked')) {
-    var html = '<span title="' + title + '">' + title + '</span>';
-    $('.multiSel').append(html);
-    $(".hida").hide();
-  } else {
-    $('span[title="' + title + '"]').remove();
-    var ret = $(".hida");
-    $('.dropdown dt a').append(ret);
-
-  }
-});
+              $elm.on('click', function() {
+                  $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top}, "slow");
+              });
+          }
+      }})
